9b227a0d42b867ff558fb6a8271764108d276d32
unknown <cg00360@bui-fa-nb.cervedgroup.com>
2015-01-25 22:25:24 +0100

    Update server side

--- smarthome_srv/index.php
+++ smarthome_srv/index.php
@@ -12,7 +12,7 @@ $app = new \Slim\Slim();
 // (Singleton resources retrieve the same log resource definition each time)
 $app->container->singleton('log', function () {
     $log = new \Monolog\Logger('SmartHome Log');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('logs/app.log', \Monolog\Logger::DEBUG));
     return $log;
 });
 
