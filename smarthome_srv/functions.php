728fe1ae6ab5473f75b4a234c21c504c1fe1049d
unknown <cg00360@bui-fa-nb.cervedgroup.com>
2015-01-25 22:15:33 +0100

    Update server side

---
+++ smarthome_srv/functions.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+  General functions
+*/
+
+
+function readMsg($fp) {
+  $i = 0;
+  $msg = '';
+  $read = '';
+  while (($read != "\r") && ($i<80)) {
+    $read = fread($fp, 1);
+    if ($read != "\r") $msg .= $read;
+    $i++;
+  }
+  $data = explode("|", $msg);
+  $ret = array();
+  if (is_array($data) && (count($data)==6)) {
+    foreach ($data as $value) {
+      list($k, $v) = split("=",$value);
+      $ret[$k] = $v;  
+    }
+    $ret['msg'] = $msg;
+    return $ret; 
+  } else {
+    return false;
+  }
+}
+
+
+function readStream(){
+  $fp =fopen("/dev/ttyUSB0", "r");
+  if( !$fp) {
+        echo "Error";die();
+  }
+  $i = 0;
+  while ((($data = readMsg($fp))==false) && ($i<3)) $i++;
+  fclose($fp);
+  return $data; 
+}
+
+
+function decodeMsgIn($msgIn) {
+	$cmd = substr($msgIn, 0, 1);
+	$val = substr($msgIn, 1);
+	switch ($cmd) {
+		case 'H':
+			  $ret = 'Tmax='.$val; 
+			break;
+		case 'L':
+			  $ret = 'Tmin='.$val; 
+			break;
+		case 'S':
+			  $ret = 'S=';
+			  $ret .= ($val=='1') ? 'summer' : 'winter'; 
+			break;
+		case 'F':
+			  $ret = 'FAN=';
+			  $ret .= ($val=='1') ? 'ON' : 'OFF'; 
+			break;
+		case 'M':
+			  $ret = 'MS=';
+			  $ret .= ($val=='1') ? 'ON' : 'OFF'; 
+			break;
+	}
+	return $ret;
+}
+
+function writeStream($msgIn) {
+  $app = \Slim\Slim::getInstance();
+  $fp =fopen("/dev/ttyUSB0", "w");
+  if( !$fp) {
+        echo "Error";die();
+  }
+  $data = readMsg($fp);  // first time read
+  $i = 0;
+  $checkCmd = false; 
+  while (($checkCmd==false) && ($i<5)) {
+    sleep(2); // waiting for Arduino resetting
+    fwrite($fp, $msgIn."\r");
+    $data = readMsg($fp);
+    $decodeMsg = decodeMsgIn($msgIn);
+    list($k, $v) = split("=",$decodeMsg);
+    $checkCmd = ($data[$k]==$v);
+    $i++;
+    $app->log->info("SmartHome 'writeStream' iteration: $i, msg in: $msgIn, decode msg: $decodeMsg");
+    if ($checkCmd) $app->log->info("SmartHome 'writeStream' iteration: $i - message sent to Arduino");
+  }  
+  fclose($fp);
+  return true;
+}
+
+
+?>
