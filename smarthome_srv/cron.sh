2843b19c29ddfb0126d95ace34862a55088f5f28
fabiobui <cg00360@bui-fa-nb.cervedgroup.com>
2015-05-14 15:06:08 +0200

    update cron

--- smarthome_srv/cron.sh
+++ smarthome_srv/cron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /var/www/nostradomus/smarthome_srv/thingspeak
-/var/www/nostradomus/smarthome_srv/thingspeak2
+/var/www/nostradomus/smarthome_srv/thingspeak3
+/var/www/nostradomus/smarthome_srv/thingspeak4
 /var/www/nostradomus/smarthome_srv/cloudapp
